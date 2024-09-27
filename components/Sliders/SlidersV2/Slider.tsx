93e1f2c6ddbb567e12ca42550573376402ccc69e
Danish <s.danish0827@gmail.com>
2024-09-27 12:17:22 +0530

    first commitdfds

--- components/Sliders/SlidersV2/Slider.tsx
+++ components/Sliders/SlidersV2/Slider.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -9,23 +9,22 @@ import "swiper/css/free-mode";
 import "swiper/css/navigation";
 import "swiper/css/thumbs";
 
-import "./styles.css";
-
 // import required modules
 import { FreeMode, Navigation, Thumbs } from "swiper/modules";
 import { siyafySlider } from "@/lib/headerData";
+import { Swiper as SwiperType } from "swiper"; // Import the Swiper type
 
 export default function App() {
   const { IsSlider, Slider } = siyafySlider;
-  const [thumbsSwiper, setThumbsSwiper] = useState(null);
+  const [thumbsSwiper, setThumbsSwiper] = useState<SwiperType | null>(null); // Set the type to Swiper or null
 
   return (
     <>
       <Swiper
-        style={{
-          "--swiper-navigation-color": "#fff",
-          "--swiper-pagination-color": "#fff",
-        }}
+        // style={{
+        //   "--swiper-navigation-color": "#fff",
+        //   "--swiper-pagination-color": "#fff",
+        // }}
         loop={true}
         spaceBetween={10}
         navigation={true}
@@ -33,14 +32,15 @@ export default function App() {
         modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper2"
       >
-        {Slider.map((media) => (
-          <SwiperSlide style={{padding:'0'}} >
-            <img src={media.image} />
+        {Slider.map((media, index) => (
+          <SwiperSlide key={index} style={{ padding: "0" }}>
+            <img src={media.image} alt={`Slide ${index + 1}`} />
           </SwiperSlide>
         ))}
       </Swiper>
+
       <Swiper
-        onSwiper={setThumbsSwiper}
+        onSwiper={setThumbsSwiper} // This will now correctly handle the Swiper instance
         loop={true}
         spaceBetween={10}
         slidesPerView={4}
@@ -49,9 +49,9 @@ export default function App() {
         modules={[FreeMode, Navigation, Thumbs]}
         className="mySwiper"
       >
-        {Slider.map((media) => (
-          <SwiperSlide>
-            <img src={media.image} />
+        {Slider.map((media, index) => (
+          <SwiperSlide key={index}>
+            <img src={media.image} alt={`Thumb ${index + 1}`} />
           </SwiperSlide>
         ))}
       </Swiper>
