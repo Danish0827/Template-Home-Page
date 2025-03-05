7cee9b39ab534f147d04a65a3c1f2dc8fd97e7b7
Danish <s.danish0827@gmail.com>
2025-03-05 08:48:24 +0530

    working on discount part

--- components/CheckOut/form-elements/Abbr.js
+++ components/CheckOut/form-elements/Abbr.js
@@ -1,19 +1,27 @@
-import PropTypes from 'prop-types';
+import PropTypes from "prop-types";
 
-const Abbr = ({required}) => {
-    if ( !required ) {
-    	return null;
-    }
+const Abbr = ({ required }) => {
+  if (!required) {
+    return null;
+  }
 
-    return <abbr className="text-red-500" style={{textDecoration: 'none'}} title="required">*</abbr>
-}
+  return (
+    <abbr
+      className="text-red-500"
+      style={{ textDecoration: "none" }}
+      title="required"
+    >
+      *
+    </abbr>
+  );
+};
 
 Abbr.propTypes = {
-    required: PropTypes.bool
-}
+  required: PropTypes.bool,
+};
 
 Abbr.defaultProps = {
-    required: false
-}
+  required: false,
+};
 
-export default Abbr
+export default Abbr;
