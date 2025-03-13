e9010b903f00e517737c4a2d0ae7cea91d2d784b
Danish <s.danish0827@gmail.com>
2025-03-13 09:37:49 +0530

    banners updated

--- components/ProductSingle/Wrapper.tsx
+++ components/ProductSingle/Wrapper.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Wrapper = ({ children, className }:any) => {
+const Wrapper = ({ children, className }: any) => {
   return (
     <>
       <div className={`w-full  px-5 md:px-10 mx-auto ${className || ""}`}>
